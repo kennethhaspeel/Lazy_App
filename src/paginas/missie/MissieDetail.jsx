8e3db6a6601e6548e8a25ca6976b7c91a79d4b14
kenneth@hiteco.be <kenneth@hiteco.be>
2024-01-21 17:25:48 +0100

    toggleDeelnemer en toggleOrganisator

--- src/paginas/missie/MissieDetail.jsx
+++ src/paginas/missie/MissieDetail.jsx
@@ -7,8 +7,9 @@ import useAuth from "../../hooks/useAuth"
 import { loader } from "react-global-loader"
 import Form from 'react-bootstrap/Form'
 import { Button, Col, InputGroup, Row } from 'react-bootstrap'
+import { Modal } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFloppyDisk,faTrashCan,faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faFloppyDisk, faTrashCan, faPlus, faSquarePlus } from '@fortawesome/free-solid-svg-icons'
 
 const MissieDetail = () => {
   const showLoader = () => {
@@ -27,6 +28,8 @@ const MissieDetail = () => {
   const [allesInOrde, setAllesInOrde] = useState(true)
   const [foutmelding, setFoutmelding] = useState([])
   const [allesBewaard, setAllesBewaard] = useState(false)
+  const [showModalSelectDeelnemer, setShowModalSelectDeelnemer] = useState(false)
+  const [showModalSelectOrganisator, setShowModalSelectOrganisator] = useState(false)
 
   const [missie, setMissie] = useState()
   const [isOrganisator, setIsOrganisator] = useState(false)
@@ -38,7 +41,7 @@ const MissieDetail = () => {
   const [users, setUsers] = useState([])
   const [startdatum, setStartdatum] = useState(format(new Date(), 'yyyy-MM-dd'))
   const [einddatum, setEinddatum] = useState(format(new Date(), 'yyyy-MM-dd'))
-  const [saveEdits,setSaveEdits] = useState(false)
+  const [saveEdits, setSaveEdits] = useState(false)
 
   useEffect(() => {
 
@@ -46,11 +49,98 @@ const MissieDetail = () => {
     const controller = new AbortController();
     const getUsers = async () => {
       try {
-        const response = await axiosPrivate.get(`${axiosUrls('MissieDetail')}?missieid=${missieid}`, {
-          signal: controller.signal
-        });
-        console.log(response.data)
+        // const response = await axiosPrivate.get(`${axiosUrls('MissieDetail')}?missieid=${missieid}`, {
+        //   signal: controller.signal
+        // });
+        // console.log(response.data)
         //setData(response.data);
+        let response = {
+          data:
+          {
+            "id": 2,
+            "titel": "titel",
+            "omschrijving": "omschrijving",
+            "startDatum": "2024-05-01T02:00:00+02:00",
+            "eindDatum": "2024-05-03T02:00:00+02:00",
+            "locatie": "locatie",
+            "publiekZichtbaar": false,
+            "gebruikers": [
+              {
+                "id": "78d06532-bc64-48be-974b-d4f4642d195a",
+                "volledigeNaam": "Bjorn Leleu",
+                "isDeelnemer": false,
+                "isOrganisator": false
+              },
+              {
+                "id": "2bc823e6-ebec-452e-b9a1-df59b9b2f481",
+                "volledigeNaam": "Fabrice Hoornaert",
+                "isDeelnemer": false,
+                "isOrganisator": false
+              },
+              {
+                "id": "5c117577-ed57-412c-bc8d-a0c19be059d0",
+                "volledigeNaam": "Kenneth Carrein",
+                "isDeelnemer": false,
+                "isOrganisator": false
+              },
+              {
+                "id": "f25cd2b4-0a22-4b09-a172-1d6a810bedd5",
+                "volledigeNaam": "Kenneth Haspeel",
+                "isDeelnemer": true,
+                "isOrganisator": true
+              },
+              {
+                "id": "d60bbf2b-1e64-4e63-a602-afbe9a9b9778",
+                "volledigeNaam": "Kim Leleu",
+                "isDeelnemer": false,
+                "isOrganisator": false
+              },
+              {
+                "id": "99defbaa-3721-47bd-9261-90ec3af6285c",
+                "volledigeNaam": "Kobe Cannière ",
+                "isDeelnemer": false,
+                "isOrganisator": false
+              },
+              {
+                "id": "90ee2e65-27cb-4c50-a298-8b1f05c77fc2",
+                "volledigeNaam": "Maxime Verpoort",
+                "isDeelnemer": false,
+                "isOrganisator": false
+              },
+              {
+                "id": "b18e9086-1e50-4bff-ab46-b8851c125eba",
+                "volledigeNaam": "Sam Six",
+                "isDeelnemer": false,
+                "isOrganisator": false
+              },
+              {
+                "id": "e385b1ad-9fa0-4ccd-a27b-38c64b2d5a8e",
+                "volledigeNaam": "Sven Masschelein",
+                "isDeelnemer": false,
+                "isOrganisator": false
+              },
+              {
+                "id": "a4bb6cd3-f8ad-468f-bc18-37ca7b93c4f1",
+                "volledigeNaam": "Test Account",
+                "isDeelnemer": true,
+                "isOrganisator": true
+              },
+              {
+                "id": "db204a5b-d190-47f8-8a28-59a3238a27f3",
+                "volledigeNaam": "Timothy Volcke",
+                "isDeelnemer": false,
+                "isOrganisator": false
+              },
+              {
+                "id": "36b58803-dd18-4a15-947d-6c90320aeee1",
+                "volledigeNaam": "jurgen Vandamme",
+                "isDeelnemer": true,
+                "isOrganisator": true
+              }
+            ]
+          }
+
+        }
         setTitel(response.data.titel)
         setOmschrijving(response.data.omschrijving)
         setStartdatum(format(response.data.startDatum, 'yyyy-MM-dd'))
@@ -58,7 +148,7 @@ const MissieDetail = () => {
         setLocatie(response.data.locatie)
         setUsers(response.data.gebruikers)
         response.data.gebruikers.forEach(u => {
-          if (currentUser.id === u.id) {       
+          if (currentUser.id === u.id) {
             setIsOrganisator(u.isOrganisator)
             setIsDeelnemer(u.isDeelnemer)
             return
@@ -82,28 +172,37 @@ const MissieDetail = () => {
     e.preventDefault()
   }
 
-  const ToggleOrganisator = (userid)=>{
+  const ToggleOrganisator = (userid) => {
     const list = users.map(obj => {
       if (obj.id === userid) {
-          return { ...obj, isOrganisator: !obj.isOrganisator }
+        return { ...obj, isOrganisator: !obj.isOrganisator }
       }
       return obj
-  })
-  setUsers(list)
+    })
+    setUsers(list)
+    setSaveEdits(true)
   }
 
-  const ToggleDeelnemer = (userid)=>{
+  const ToggleDeelnemer = (userid) => {
     const list = users.map(obj => {
       if (obj.id === userid) {
-          return { ...obj, isDeelnemer: !obj.isDeelnemer }
+        return { ...obj, isDeelnemer: !obj.isDeelnemer }
       }
       return obj
-  })
-  setUsers(list)
+    })
+    setUsers(list)
+    setSaveEdits(true)
+  }
+
+  const handleCloseSelectDeelnemer = () => {
+    setShowModalSelectDeelnemer(false)
+  }
+  const handleCloseSelectOrganisator = () => {
+    setShowModalSelectOrganisator(false)
   }
   return (
     <>
-      <h2>Details Missie: {titel}</h2>
+      <h2>Details Missie: {titel} <Button variant="success" disabled={!saveEdits}><FontAwesomeIcon icon={faFloppyDisk} /> Bewaar</Button></h2>
       {
         isOrganisator ? (
           <form onSubmit={handleSubmit} className='mb-4'>
@@ -113,7 +212,7 @@ const MissieDetail = () => {
                 <Form.Control
                   id="formTitel"
                   type="text"
-                  onChange={(e) => {setTitel(e.target.value);setSaveEdits(true)}}
+                  onChange={(e) => { setTitel(e.target.value); setSaveEdits(true) }}
                   readOnly={isOrganisator ? false : true}
                   value={titel}
                 />
@@ -125,7 +224,7 @@ const MissieDetail = () => {
                 <Form.Control
                   id="formLocatie"
                   type="text"
-                  onChange={(e) => {setLocatie(e.target.value);setSaveEdits(true)}}
+                  onChange={(e) => { setLocatie(e.target.value); setSaveEdits(true) }}
                   readOnly={isOrganisator ? false : true}
                   value={locatie}
                 />
@@ -137,7 +236,7 @@ const MissieDetail = () => {
                 <Form.Control as="textarea"
                   id="formOmschrijving"
                   rows={3}
-                  onChange={(e) => {setOmschrijving(e.target.value);setSaveEdits(true)}}
+                  onChange={(e) => { setOmschrijving(e.target.value); setSaveEdits(true) }}
                   readOnly={isOrganisator ? false : true}
                   value={omschrijving}
                 />
@@ -150,7 +249,7 @@ const MissieDetail = () => {
                 <Form.Control
                   id="formDatumStart"
                   type="date"
-                  onChange={(e) => {setStartdatum(e.target.value);setSaveEdits(true)}}
+                  onChange={(e) => { setStartdatum(e.target.value); setSaveEdits(true) }}
                   readOnly={isOrganisator ? false : true}
                   value={startdatum}
                 />
@@ -160,32 +259,25 @@ const MissieDetail = () => {
             <Form.Group as={Row} className="mb-3">
               <Form.Label column md={2} sm={12}>Einddatum</Form.Label>
               <Col md={6} sm={12}>
-
                 <Form.Control
                   id="formDatumEinde"
                   type="date"
-                  onChange={(e) => {setEinddatum(e.target.value);setSaveEdits(true)}}
+                  onChange={(e) => { setEinddatum(e.target.value); setSaveEdits(true) }}
                   readOnly={isOrganisator ? false : true}
                   value={einddatum}
                 />
-
               </Col>
             </Form.Group>
-            <hr/>
+            <hr />
             <Row className="mb-3">
-              <Col md={2} sm={12} >Organisatoren <Button variant="info"><FontAwesomeIcon icon={faPlus} /></Button></Col>
+              <Col md={2} sm={12} >Organisatoren <Button variant="info"><FontAwesomeIcon icon={faPlus} onClick={() => { setShowModalSelectOrganisator(true) }} /></Button></Col>
               <Col md={8} sm={12}>
                 {
                   users.map((u) => (
                     u.isOrganisator ? (
                       <>
-                        <Button 
-                        key={u.id}
-                        variant="danger" 
-                        className='me-2 mb-2'
-                        onClick={()=>{ToggleOrganisator(u.id)}}
-                        >
-                        <FontAwesomeIcon icon={faTrashCan} /> {u.volledigeNaam}
+                        <Button key={u.id} variant="danger" className='me-2 mb-2' onClick={() => { ToggleOrganisator(u.id) }}>
+                          <FontAwesomeIcon icon={faTrashCan} /> {u.volledigeNaam}
                         </Button>
                       </>
                     ) : ('')
@@ -193,24 +285,24 @@ const MissieDetail = () => {
                 }
               </Col>
             </Row>
-            <hr/>
+            <hr />
             <Row className="mb-3">
               <Col md={2} sm={12} >
-                  Deelnemers <Button variant="info"><FontAwesomeIcon icon={faPlus} /></Button>
-                </Col>
+                Deelnemers <Button variant="info"><FontAwesomeIcon icon={faPlus} onClick={() => setShowModalSelectDeelnemer(true)} /></Button>
+              </Col>
               <Col md={8} sm={12}>
                 {
                   users.map((u) => (
 
                     (u.isDeelnemer && !u.isOrganisator) ? (
                       <>
-                        <Button 
-                        key={u.id}
-                        variant="info" 
-                        className='me-2 mb-2'
-                        onClick={()=>{ToggleDeelnemer(u.id)}}
+                        <Button
+                          key={u.id}
+                          variant="info"
+                          className='me-2 mb-2'
+                          onClick={() => { ToggleDeelnemer(u.id) }}
                         >
-                        <FontAwesomeIcon icon={faTrashCan} /> {u.volledigeNaam}
+                          <FontAwesomeIcon icon={faTrashCan} /> {u.volledigeNaam}
                         </Button>
                       </>
                     ) : ('')
@@ -218,19 +310,7 @@ const MissieDetail = () => {
                 }
               </Col>
             </Row>
-            <hr/>
-            <Row className='pt-1'>
-              <Col md={{ span: 2, offset: 2 }} sm={{ span: 6, offset: 3 }}>
-                <div className="d-grid gap-2">
-                  <Button 
-                  variant="success" 
-                  disabled={!saveEdits}><FontAwesomeIcon icon={faFloppyDisk} /> Bewaar</Button>
-                </div>
-              </Col>
-
-            </Row>
             <hr />
-
           </form>
 
         ) : (
@@ -303,6 +383,60 @@ const MissieDetail = () => {
         )
       }
 
+      <Modal show={showModalSelectDeelnemer} onHide={handleCloseSelectDeelnemer}>
+        <Modal.Header closeButton>
+          <Modal.Title>Deelnemer toevoegen</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {
+            users.map(user => (
+              !user.isDeelnemer ?
+                (
+                  <Row key={user.id}>
+                    <Col className="d-grid gap-2 pb-2">
+                      <Button variant="success" onClick={() => { ToggleDeelnemer(user.id) }}>
+                        <FontAwesomeIcon icon={faSquarePlus} /> {user.volledigeNaam}
+                      </Button>
+                    </Col>
+                  </Row>
+                )
+                : ('')
+            ))
+          }
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseSelectDeelnemer}>
+            Sluit
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={showModalSelectOrganisator} onHide={handleCloseSelectOrganisator}>
+        <Modal.Header closeButton>
+          <Modal.Title>Deelnemer toevoegen</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {
+            users.map(user => (
+              (!user.isOrganisator && user.isDeelnemer) ?
+                (
+                  <Row key={user.id}>
+                    <Col className="d-grid gap-2 pb-2">
+                      <Button variant="success" onClick={() => { ToggleOrganisator(user.id) }}>
+                        <FontAwesomeIcon icon={faSquarePlus} /> {user.volledigeNaam}
+                      </Button>
+                    </Col>
+                  </Row>
+                )
+                : ('')
+            ))
+          }
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseSelectOrganisator}>
+            Sluit
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
 
   )

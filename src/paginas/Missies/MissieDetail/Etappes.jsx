01ac96093e698880a6c10097bebf343a4d171fdb
kenneth@hiteco.be <kenneth@hiteco.be>
2024-01-30 18:53:48 +0100

    etappes en kosten

--- src/paginas/Missies/MissieDetail/Etappes.jsx
+++ src/paginas/Missies/MissieDetail/Etappes.jsx
@@ -1,36 +1,37 @@
-import { GetMissieDagen, DateToDDMMYYYY } from "../../../components/DatumFuncties"
+import { GetMissieDagen, DateToDDMMYYYY, CompareDates } from "../../../components/DatumFuncties"
 import { useState, useEffect } from "react"
 import { axiosUrls } from '../../../api/axios'
 import useAxiosPrivate from '../../../hooks/useAxiosPrivate'
-import { Alert } from "react-bootstrap"
+import { Alert, Col, ListGroup, Row } from "react-bootstrap"
 
-const EtappeComponent = ({ etappes, setEtappes, isOrganisator, missiedetail ,missieid}) => {
+const EtappeComponent = ({ etappes, setEtappes, isOrganisator, missiedetail, missieid }) => {
   const axiosPrivate = useAxiosPrivate();
   const [missiedagen, setMissiedagen] = useState([])
-const [nieuweEtappe, setNieuweEtappe]= useState({})
-const [isLoading, setIsLoading] = useState(true)
+  const [nieuweEtappe, setNieuweEtappe] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
 
 
-useEffect(() => {
-const getMissieEtappes = async () => {
+  useEffect(() => {
+    const getMissieEtappes = async () => {
 
-  try {
-    const response = await axiosPrivate.get(`${axiosUrls('GetMissieEtappes')}?missieid=${missieid}`);
-    setEtappes(response.data)
-    console.log(response.data)
-    setIsLoading(false)
-    
-  } catch (err) {
-    console.error(err);
-  }
-}
+      try {
+        const response = await axiosPrivate.get(`${axiosUrls('GetMissieEtappes')}?missieid=${missieid}`);
+        setEtappes(response.data)
+        console.log(response.data)
+        setIsLoading(false)
 
-getMissieEtappes()
-}, [])
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    getMissieEtappes()
+  }, [])
 
 
   useEffect(() => {
     setMissiedagen(GetMissieDagen(missiedetail.startDatum, missiedetail.eindDatum))
+    console.log(missiedagen)
   }, [missiedetail])
 
 
@@ -46,6 +47,34 @@ getMissieEtappes()
               <Alert key={index} variant='light'>
                 {index === 0 ? ('Algemeen') : DateToDDMMYYYY(dag)}
               </Alert>
+              <ListGroup variant="flush" className="pb-4 ps-4">
+                {
+                  etappes.length ? (
+
+                    etappes
+                      .filter(etappe => { return CompareDates(dag, etappe.startDatum) })
+                      .map(etappe => (
+
+                        <ListGroup.Item key={etappe.id}>
+                          <Row>
+                            <Col>
+                              {etappe.titel}
+                            </Col>
+                            <Col>
+                              {etappe.locatie}
+                            </Col>
+                            <Col className="text-end pe-3">
+                              {Math.floor(etappe.bedrag).toFixed(2)}
+                            </Col>
+                          </Row>
+                        </ListGroup.Item>
+
+
+                      ))
+                  ) : ('Nog geen etappes')
+                }
+              </ListGroup>
+              <hr />
             </div>
           ))
         ) : (

0731b3062267e4a85ef6e2bac2394527ec040183
Kenneth Haspeel <Kenneth.Haspeel@wervik.be>
2024-05-07 15:42:36 +0200

    verfijning etappeoverzicht

--- src/paginas/Missies/MissieDetail/EtappeOverzicht.jsx
+++ src/paginas/Missies/MissieDetail/EtappeOverzicht.jsx
@@ -1,4 +1,4 @@
-import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { useQuery,  } from '@tanstack/react-query'
 import useAxiosPrivate from "../../../hooks/useAxiosPrivate";
 import { axiosUrls } from "../../../api/axios";
 import { ErrorBoundary } from 'react-error-boundary';
@@ -13,7 +13,6 @@ import { useNavigate } from 'react-router-dom';
 const EtappeOverzicht = ({ missieId, startDatum, eindDatum, totaalKost, setTotaalKost }) => {
     const navigate = useNavigate()
     const axiosPrivate = useAxiosPrivate();
-    const queryClient = useQueryClient();
     const [missiedata, setMissiedata] = useState([])
 
     let totaleKost = 0
@@ -23,17 +22,15 @@ const EtappeOverzicht = ({ missieId, startDatum, eindDatum, totaalKost, setTotaa
             const response = await axiosPrivate.get(
                 `${axiosUrls("GetMissieEtappes")}?missieid=${missieId}`
             );
-            console.log(response.data);
             setMissiedata(GetMissieDagen(startDatum, eindDatum))
             return response.data;
         },
     })
 
     useEffect(() => {
-        etappes.map((e) => {
-            totaleKost += e.kost
+        etappes?.map((e) => {
+          return  totaleKost += e.kost
         })
-        console.log(totaleKost)
         setTotaalKost(totaleKost.toFixed(2))
     }, [etappes])
 
@@ -53,19 +50,19 @@ const EtappeOverzicht = ({ missieId, startDatum, eindDatum, totaalKost, setTotaa
                                         </ListGroup.Item>
                                         <ListGroup.Item key={'listgroupitem_01'} variant='secondary'>
                                             <Row>
-                                                <Col xs={6} lg={4}>Titel</Col>
-                                                <Col xs={6} lg={2} className='text-center'>
+                                                <Col xs={6} lg={4}  key={`listgroup_titel_${index}`}>Titel</Col>
+                                                <Col xs={6} lg={2}  key={`listgroup_fotos_${index}`}className='text-center'>
                                                     Foto's
                                                 </Col>
-                                                <Col xs={6} lg={3} className='text-center'>
+                                                <Col xs={6} lg={3}  key={`listgroup_financieel_${index}`}className='text-center'>
                                                     Financieel
                                                 </Col>
 
-                                                <Col xs={6} lg={3}>Betaler</Col>
+                                                <Col xs={6} lg={3}  key={`listgroup_betaler_${index}`}>Betaler</Col>
 
                                             </Row>
                                         </ListGroup.Item>
-                                        {
+                                         {
                                             etappes
                                                 .filter((etappe) => {
                                                     return CompareDates(dag, etappe.datumTijd);
@@ -79,29 +76,29 @@ const EtappeOverzicht = ({ missieId, startDatum, eindDatum, totaalKost, setTotaa
                                                             <>
                                                                 <ListGroup.Item key={`listgroupitem_${et.id.toString()}`}>
                                                                     <Row>
-                                                                        <Col xs={6} lg={4}>{et.titel}</Col>
-                                                                        <Col xs={6} lg={2} className='text-center'>
+                                                                        <Col xs={6} lg={4} key={`col_titel_${et.id.toString()}`}>{et.titel}</Col>
+                                                                        <Col xs={6} lg={2} key={`col_fotos_${et.id.toString()}`} className='text-center'>
                                                                             <ButtonGroup>
-                                                                                <Button variant={et.aantalEtappeFotos > 0 ? 'success' : 'outline-secondary'}><FontAwesomeIcon icon={faImages} />&nbsp;{et.aantalEtappeFotos}</Button>
-                                                                                <Button variant='success'><FontAwesomeIcon icon={faPlus} /></Button>
+                                                                                <Button  key={`btn_aantalfotos_${et.id.toString()}`} variant={et.aantalEtappeFotos > 0 ? 'success' : 'outline-secondary'}><FontAwesomeIcon icon={faImages} />&nbsp;{et.aantalEtappeFotos}</Button>
+                                                                                <Button key={`btn_bekijkfotos_${et.id.toString()}`} variant='success'><FontAwesomeIcon icon={faPlus} /></Button>
                                                                             </ButtonGroup>
                                                                         </Col>
-                                                                        <Col xs={6} lg={3} className='border-left'>
+                                                                        <Col xs={6} lg={3}  key={`col_kosten_${et.id.toString()}`} className='border-left'>
                                                                             <Row>
-                                                                                <Col className='text-end pe-2'>
+                                                                                <Col className='text-end pe-2'  key={`col_totalekost_${et.id.toString()}`}>
                                                                                     &euro; {et.kost.toFixed(2)} {() => { setTotaalKost(totaalKost + et.kost) }}
                                                                                 </Col>
-                                                                                <Col>
+                                                                                <Col  key={`col_bewijsstuk_${et.id.toString()}`}>
                                                                                     <ButtonGroup>
-                                                                                        <Button variant={et.aantalKostBewijsstukken > 0 ? 'success' : 'outline-secondary'}><FontAwesomeIcon icon={faFile} />&nbsp;{et.aantalKostBewijsstukken}</Button>
-                                                                                        <Button variant='success'><FontAwesomeIcon icon={faPlus} /></Button>
+                                                                                        <Button  key={`btn_aantalbewijsstukken_${et.id.toString()}`} variant={et.aantalKostBewijsstukken > 0 ? 'success' : 'outline-secondary'}><FontAwesomeIcon icon={faFile} />&nbsp;{et.aantalKostBewijsstukken}</Button>
+                                                                                        <Button key={`btn_toonbewijsstukken_${et.id.toString()}`} variant='success'><FontAwesomeIcon icon={faPlus} /></Button>
                                                                                     </ButtonGroup>
 
                                                                                 </Col>
                                                                             </Row>
                                                                         </Col>
 
-                                                                        <Col xs={6} lg={3}>{et.betaaldDoor}</Col>
+                                                                        <Col  key={`col_betaler_${et.id.toString()}`} xs={6} lg={3}>{et.betaaldDoor}</Col>
 
                                                                     </Row>
 
@@ -118,7 +115,7 @@ const EtappeOverzicht = ({ missieId, startDatum, eindDatum, totaalKost, setTotaa
 
                                             )
 
-                                        }
+                                        } 
                                     </ListGroup>
                                 </div>
                             )

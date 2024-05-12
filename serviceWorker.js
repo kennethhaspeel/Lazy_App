f59c860a7b03602d0a4ef3b0318190bcd36c3168
kenneth@hiteco.be <kenneth@hiteco.be>
2024-05-12 16:58:48 +0200

    Updates

---
+++ serviceWorker.js
@@ -0,0 +1,16 @@
+﻿console.log('Hello from serviceWorker.js');
+var CACHE_NAME = 'pwa-task-manager';
+var urlsToCache = [
+  '/'
+];
+
+self.addEventListener('install', event => {
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
